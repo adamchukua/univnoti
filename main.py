fa7325c95714fc50efa978714e028412a6736fb6
thegradle <adamchuk.v.1@gmail.com>
2021-11-05 17:35:53 +0200

    Divide files, refactoring, fix reminder

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from tgtoken import TOKEN
+from lessons import lessons
+from scheduleList import scheduleList
+from reminderTimes import reminderTimes
 import telebot
 import datetime
 import schedule
-import tgtoken
 from threading import Thread
 from time import sleep
 
-bot = telebot.TeleBot(tgtoken.TOKEN)
+bot = telebot.TeleBot(TOKEN)
 typeOfWeek = (datetime.datetime.today().isocalendar()[1] - datetime.date(2021, 10, 29).isocalendar()[1]) % 2
 dayOfWeek = datetime.datetime.today().weekday()
 
@@ -19,140 +22,6 @@ weekdays = {
     6: "Неділя"
 }
 
-times = [
-    "08:55",
-    "10:25",
-    "12:25",
-    "13:55",
-    "15:25",
-    "16:55",
-    "18:25"
-]
-
-lessons = {
-    "англ": ["Англійська мова", "meet.google.com/ygy-phic-sia"],
-    "кгозл": ["Комп'ютерна графіка та обробка зображень (лекція)", "meet.google.com/cat-tnso-ydp"],
-    "кгозп": ["Комп'ютерна графіка та обробка зображень (практика)", "постійне посилання відсутнє"],
-    "веб": ["Web-програмування", "meet.google.com/cat-tnso-ydp"],
-    "ткпл": ["Технології комп'ютерного проєктування (лекція)", "meet.google.com/tue-zwcs-wtc"],
-    "ткпп": ["Технології комп'ютерного проєктування (практика)", "meet.google.com/xui-xeti-obn"],
-    "алгл": ["Алгоритми та структури даних (лекція)", "meet.google.com/wzr-abyj-bas"],
-    "алгп": ["Алгоритми та структури даних (практика)", "meet.google.com/abx-gsop-fey"],
-    "ооп": ["Об'єктно-орієнтоване проєктування", "meet.google.com/yjv-qxit-wfx"],
-    "тймсл": ["Теорія ймовірності та математична статистика (лекція)", "постійне посилання відсутнє"],
-    "тймсп": ["Теорія ймовірності та математична статистика (практика)", "постійне посилання відсутнє"],
-    "нп": ["Нефаховий предмет", "постійне посилання відсутнє"],
-    "фв": ["Фізичне виховання", "постійне посилання відсутнє"]
-}
-
-scheduleList = [
-    [
-        [
-            [
-                lessons["алгп"]
-            ],
-            [
-                lessons["ткпп"]
-            ],
-            [
-                lessons["веб"]
-            ]
-        ],
-        [],
-        [
-            [
-                lessons["ткпл"]
-            ],
-            [
-                lessons["алгл"]
-            ],
-            [
-                lessons["ооп"]
-            ]
-        ],
-        [
-            [
-                lessons["кгозп"]
-            ],
-            [
-                lessons["веб"]
-            ],
-            [
-                lessons["нп"]
-            ]
-        ],
-        [
-            [
-                lessons["ооп"]
-            ],
-            [
-                lessons["англ"]
-            ],
-            [
-                lessons["ткпп"]
-            ],
-            [
-                lessons["фв"]
-            ]
-        ],
-        [],
-        []
-    ],
-    [
-        [
-            [
-                lessons["алгп"]
-            ],
-            [
-                lessons["тймсп"]
-            ],
-            [
-                lessons["веб"]
-            ]
-        ],
-        [],
-        [
-            [
-                lessons["тймсл"]
-            ],
-            [
-                lessons["алгл"]
-            ],
-            [
-                lessons["ооп"]
-            ]
-        ],
-        [
-            [],
-            [
-                lessons["кгозл"]
-            ],
-            [
-                lessons["нп"]
-            ],
-            [
-                lessons["нп"]
-            ]
-        ],
-        [
-            [
-                lessons["ооп"]
-            ],
-            [
-                lessons["англ"]
-            ],
-            [
-                lessons["ткпп"]
-            ],
-            [
-                lessons["фв"]
-            ]
-        ],
-        [],
-        []
-    ]
-]
-
 
 def day_lessons(day):
     result = "*" + weekdays[day] + " (тиждень " + str(typeOfWeek + 1) + ")*\n\n"
@@ -160,10 +29,8 @@ def day_lessons(day):
 
     if len(scheduleList[typeOfWeek][day]) > 0:
         for lesson in scheduleList[typeOfWeek][day]:
-            try:
+            if len(lesson[0][0]) > 0:
                 result += str(i) + ". " + lesson[0][0] + " - " + lesson[0][1] + "\n"
-            except:
-                print("error: a lesson doesn't exist")
             i += 1
     else:
         result = "Нічого немає, чіллим... Жартую, у вас курсова."
@@ -174,6 +41,22 @@ def day_lessons(day):
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Цей бот нагадує про пари.")
+
+
+@bot.message_handler(commands=["help"])
+def send_help(message):
+    commands = "*Посилання на пари*\n\n"
+
+    for lesson in lessons:
+        commands += "`!" + lesson + "` - " + lessons[lesson][0].lower() + "\n"
+
+    commands += "\n*Розклад*\n\n" \
+                "`!сьогодні` - дізнатися розклад на сьогодні\n" \
+                "`!завтра` - дізнатися розклад на завтра"
+
+    bot.send_message(message.chat.id, commands,
+                     parse_mode="Markdown",
+                     disable_web_page_preview=True)
 
 
 @bot.message_handler(func=lambda message: True)
@@ -191,18 +74,7 @@ def main(message):
                          parse_mode="Markdown",
                          disable_web_page_preview=True)
     elif request == "все":
-        all = "*Посилання на пари*\n\n"
-
-        for lesson in lessons:
-            all += "`!" + lesson + "` - " + lessons[lesson][0].lower() + "\n"
-
-        all += "\n*Розклад*\n\n" \
-               "`!сьогодні` - дізнатися розклад на сьогодні\n" \
-               "`!завтра` - дізнатися розклад на завтра"
-
-        bot.send_message(message.chat.id, all,
-                         parse_mode="Markdown",
-                         disable_web_page_preview=True)
+        send_help(message)
 
 
 def schedule_checker():
@@ -220,17 +92,13 @@ def schedule_send_link(i):
 
 
 if __name__ == "__main__":
-    if len(scheduleList[typeOfWeek][dayOfWeek]) > 0:
-        try:
-            schedule.every().day.at(times[0]).do(schedule_send_link, 0)
-            schedule.every().day.at(times[1]).do(schedule_send_link, 1)
-            schedule.every().day.at(times[2]).do(schedule_send_link, 2)
-            schedule.every().day.at(times[3]).do(schedule_send_link, 3)
-            schedule.every().day.at(times[4]).do(schedule_send_link, 4)
-            schedule.every().day.at(times[5]).do(schedule_send_link, 5)
-            schedule.every().day.at(times[6]).do(schedule_send_link, 6)
-        except:
-            print("There are not any lessons today")
+    i = 0
+    numberOfLessons = len(scheduleList[typeOfWeek][dayOfWeek])
+
+    while numberOfLessons > i - 1:
+        print(i)
+        schedule.every().day.at(reminderTimes[i]).do(schedule_send_link, i)
+        i += 1
 
     Thread(target=schedule_checker).start()
 
